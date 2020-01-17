d37272f6e6f157b8c17e4d76e33c8af0402a8b84
Bradley Ramos <bradleymramos@gmail.com>
2020-01-16 20:51:05 -0600

    styled catalog page with images

--- new-shopping-cart/src/App.js
+++ new-shopping-cart/src/App.js
@@ -1,6 +1,34 @@
 import React, { useEffect, useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+
+const useStyles = makeStyles(theme => ({
+  card: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 140,
+  },
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}));
 
 const App = () => {
+  const classes = useStyles();
   const [data, setData] = useState({});
   const products = Object.values(data);
   useEffect(() => {
@@ -13,9 +41,37 @@ const App = () => {
   }, []);
 
   return (
-    <ul>
-      {products.map(product => <li key={product.sku}>{product.title}</li>)}
-    </ul>
+    <Grid container spacing={3}>
+        {products.map(product =>
+        <Grid item xs={3}>
+        <Card className={classes.card}>
+        <CardActionArea>
+          <CardMedia
+            className={classes.media}
+            image={'./data/products/'+product.sku+'_1.jpg'}
+            title={product.title}
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              {product.title}
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+              {product.description}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Button size="small" color="primary">
+            Add to cart
+          </Button>
+          <Button size="small" color="primary">
+            Learn More
+          </Button>
+        </CardActions>
+      </Card>
+      </Grid>
+        )}
+    </Grid>
   );
 };
 
